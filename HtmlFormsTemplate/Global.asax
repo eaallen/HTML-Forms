6472c744ffbe992dabefe5f8e4ab7193ff726d4e
Dan Morain <dan.morain91@gmail.com>
2015-10-21 10:31:57 -0600

    initial commit

---
+++ HtmlFormsTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HtmlFormsTemplate.MvcApplication" Language="C#" %>
---
+++ HtmlFormsTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HtmlFormsTemplate.Startup))]
+namespace HtmlFormsTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
